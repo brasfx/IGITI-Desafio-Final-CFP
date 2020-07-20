793ddfe08a8c4f56446d2848845aee2ad452dae4
davi.ribeiro <foxpc18@gmail.com>
2020-07-20 19:23:25 -0300

    Alteração rota heroku

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: `http://localhost:3001/api/transaction`,
+  baseURL: `${process.env.REACT_APP_BASE_URL}`,
 });
 
 console.log(process.env.REACT_APP_BASE_URL);
